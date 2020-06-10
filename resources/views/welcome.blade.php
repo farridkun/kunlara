11717173468510da7527726408442be5d0ba260d
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2020-06-10 21:28:23 +0000

    begining of all

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kun-Lara</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kun-Lara
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/farridkun/kunlara">Github</a>
+                    <a href="https://instagram.com/farrid_jr">Instagram</a>
+                    <a href="https://twitter.com/kuntoro_farrid">Twitter</a>
+                    <a href="https://bercanda.digital">Holdings</a>
+                    <a href="mailto:farridguntoro@gmail.com">Mail</a>
                 </div>
             </div>
         </div>
